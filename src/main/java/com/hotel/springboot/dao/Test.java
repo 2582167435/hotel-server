5b768dcddd0b2e3ed549ca3c3700216e0ecf12f6
YL <2582167435@qq.com>
2019-03-18 10:39:13 +0800

    inital

--- src/main/java/com/hotel/springboot/dao/Test.java
+++ src/main/java/com/hotel/springboot/dao/Test.java
@@ -1,4 +1,4 @@
 package com.hotel.springboot.dao;
 
-public class test {
+public interface Test {
 }
---
+++ src/main/java/com/hotel/springboot/dao/impl/TestImpl.java
@@ -0,0 +1,6 @@
+package com.hotel.springboot.dao.impl;
+
+import com.hotel.springboot.dao.Test;
+
+public class TestImpl implements Test {
+}
